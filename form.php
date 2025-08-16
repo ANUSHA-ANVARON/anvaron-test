4e4eb1f94202b2c0bb539efcaa642c73eaefd55f
sathishk2212 <53809676+sathishk2212@users.noreply.github.com>
2025-08-16 10:53:47 +0530

    initialpush

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+if ($_POST) {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $company = htmlspecialchars($_POST['company']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Email configuration
+    $to = "sathishk2212@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\nCompany: $company\nMessage: $message";
+    $headers = "From: $email";
+    
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
